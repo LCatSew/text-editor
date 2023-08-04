5f1e70dc9d6221003d5b81fb92bd269102a333a7
LCatSew <119518206+LCatSew@users.noreply.github.com>
2023-08-03 22:52:22 -0500

    added logic to put and get methods, but may need to be post

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,48 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// I feel like this may need to be a post method instead of a put method
+export const putDb = async (content) => {
+  console.log ('PUT to database');
+
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB('jate', 1);
+
+  //create a transaction on the database
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  //open the specific object store
+  const store = tx.objectStore('jate');
+
+  //add the item to the object store
+  const request = await store.add(content);
+
+  //return the result
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log ('GET from database');
+
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB('jate', 1);
+
+  //create a transaction on the database
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  //open the specific object store
+  const store = tx.objectStore('jate');
+
+  //get all the items in the object store
+  const request = await store.getAll();
+
+  //return the items
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
